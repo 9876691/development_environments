021a7fbb781dca4446388f29085388227a3659f5
Ian Purton <ian.purton@gmail.com>
2021-08-25 13:14:29 +0200

    Update aliases.bash

--- new-rust-devcontainer/aliases.bash
+++ new-rust-devcontainer/aliases.bash
@@ -7,6 +7,8 @@ alias gcam='git commit -a -m'
 alias gpsup="git push --set-upstream origin $(git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||')"
 #alias gpsup='git push --set-upstream origin $(git_current_branch)'
 alias gcb='git checkout -b'
+alias gitsetup='git config --global user.name \$NAME && git config --global user.email \$EMAIL && mkdir -p ~/.ssh && cp -u /home/host-ssh/id_rsa ~/.ssh && chmod 600 ~/.ssh/id_rsa && ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub'
+
 
 # Cargo watch
 alias cw='cargo watch --no-gitignore -i *.scss -i *.ts -i "package*" -x fmt -x clippy -x run'
@@ -20,6 +22,6 @@ alias mre='diesel migration redo'
 alias ml='diesel migration list'
 alias db='psql $DATABASE_URL'
 
-alias p='sudo chown vscode:vscode /vscode/web-ui/* && sudo chown vscode:vscode /vscode/trillian-personality/*'
+alias p='sudo chown vscode:vscode .
 # Leave a line below or the files will cat together
 
